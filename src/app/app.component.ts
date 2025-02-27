0643933248a49fcdc34963ad830a345b0736e5ae
Boris Skert <boris.skert@byteagenten.de>
2025-02-27 08:30:13 +0100

    Create first form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [
+    ReactiveFormsModule,
+    NgForOf,
+    NgIf
+  ]
 })
 export class AppComponent {
-  title = 'dynamic-form-app';
+  dynamicForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.dynamicForm = this.fb.group({
+      name: ['', Validators.required],  // Static field
+      emails: this.fb.array([]), // Dynamic array of emails
+    });
+  }
+
+  // Get the FormArray
+  get emails(): FormArray {
+    return this.dynamicForm.get('emails') as FormArray;
+  }
+
+  // Add new email field
+  addEmail() {
+    this.emails.push(this.fb.control('', [Validators.required, Validators.email]));
+  }
+
+  // Remove email field by index
+  removeEmail(index: number) {
+    this.emails.removeAt(index);
+  }
+
+  // Submit form
+  submitForm() {
+    if (this.dynamicForm.valid) {
+      console.log('Form Data:', this.dynamicForm.value);
+    }
+  }
 }
