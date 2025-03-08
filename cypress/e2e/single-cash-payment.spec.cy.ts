d388e79b0295ebe0ec8b677105a1128f0c44c6d9
Boris Skert <boris.skert@gmail.com>
2025-03-08 20:40:07 +0100

    Introduce  test

---
+++ cypress/e2e/single-cash-payment.spec.cy.ts
@@ -0,0 +1,26 @@
+describe('Single Cash Payment', () => {
+    beforeEach(() => {
+        cy.visit('/')
+    })
+
+    it('Visits the initial project page', () => {
+        cy.contains('App Component')
+    })
+
+    describe('When fill name', () => {
+        beforeEach(() => cy
+            .get('.dynamic-form--name').type('John Doe')
+            .then(() => cy
+                .get('.dynamic-form--submit').click()
+            )
+        )
+
+        it('Should show submitted form value', () => {
+            cy.get('div.submitted-value-container')
+                .should(
+                    'contain.text',
+                    '{\n  "name": "John Doe",\n  "payments": [\n    {\n      "paymentMethod": "cash",\n      "paymentDetails": null\n    }\n  ]\n}'
+                )
+        })
+    });
+})
--- cypress/e2e/spec.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('Single Cash Payment', () => {
-  it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('App Component')
-  })
-})
